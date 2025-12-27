a517e02c01a3369b7cf349ca85ff5886687f477d
Oisin <99879353+des1-gner@users.noreply.github.com>
2025-12-27 13:32:37 +1100

    Initialize AWS CDK app with SimpleMLflowStack

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+#!/usr/bin/env python3
+import aws_cdk as cdk
+from mlflow_stack import SimpleMLflowStack
 
+app = cdk.App()
+
+SimpleMLflowStack(
+    app,
+    "MLflowStack",
+    env=cdk.Environment(
+        account="<your-aws-account-id>", # <-- CHANGE THIS
+        region="<your-aws-sagemaker-domain-region>", # <-- CHANGE THIS
+    ),
+)
+
+app.synth()
